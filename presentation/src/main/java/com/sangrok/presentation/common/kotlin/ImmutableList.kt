30a877bd9016b8a7d74e483f9b8e194c728eca2e
EvergreenTree97 <tkdfhr7497@naver.com>
2023-08-24 20:05:27 +0900

    test: remove unused test

--- presentation/src/main/java/com/sangrok/presentation/common/kotlin/ImmutableList.kt
+++
@@ -1,8 +0,0 @@
-package com.sangrok.presentation.common.kotlin
-
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
-
-inline fun <T> ImmutableList<T>.copy(mutator: MutableList<T>.() -> Unit): ImmutableList<T> {
-    return toMutableList().apply(mutator).toImmutableList()
-}
